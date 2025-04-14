616954cf615023ea3d0b8c284b3d7ae9179f3d52
Bartosz <janiukbartosz@gmail.com>
2025-04-14 21:43:56 +0200

    wip

--- src/views/Home/components/Mission.tsx
+++ src/views/Home/components/Mission.tsx
@@ -1,45 +1,66 @@
+import * as React from "react";
+
 export const Mission = () => {
-	return (
-		<main className="container mx-auto">
-			<div className="responsive-padding mt-8 flex flex-col gap-16">
-				<div className="flex flex-col gap-4">
-					<h1 className="uppercase text-5xl font-bold">Misja</h1>
-					<p>
-						Celem aplikacji Trinder jest pomoc w poszukiwaniach twojego nowego,
-						wymarzonego trenera personalnego lub podopiecznego, którego będziesz
-						mógł poprowadzić w czasie jego treningu. Wybierz ścieżkę która cię
-						interesuje i zobacz jakie to proste!
-					</p>
-				</div>
-				<div className="flex gap-4">
-					<div className="max-h-[481px] bg-primary rounded-lg w-full p-4">
-						<div className="flex">
-							<label className="inline-flex items-center me-5 cursor-pointer">
-								<input type="checkbox" value="" className="sr-only peer" />
-								<div className="relative w-11 h-6 bg-transparent border border-background rounded-full peer peer-focus:ring-4 peer-focus:ring-primaryDarker peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-background peer-checked:after:bg-primary after:border-primaryLighter after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-layout "></div>
-							</label>
-							<h2 className="uppercase text-background text-4xl">TRENUJĄCY</h2>
-						</div>
-						<div className="flex justify-center items-end h-full">
-							<img className="h-[90%] " src="/assets/mission1.png" />
-						</div>
-					</div>
-					<div className="w-full flex flex-col gap-4">
-						<Step />
-						<Step />
-						<Step />
-					</div>
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <main className="container mx-auto">
+      <div className="responsive-padding mt-8 flex flex-col gap-16">
+        <div className="flex flex-col gap-4">
+          <h1 className="uppercase text-5xl font-bold">Misja</h1>
+          <p>
+            Celem aplikacji Trinder jest pomoc w poszukiwaniach twojego nowego,
+            wymarzonego trenera personalnego lub podopiecznego, którego będziesz
+            mógł poprowadzić w czasie jego treningu. Wybierz ścieżkę która cię
+            interesuje i zobacz jakie to proste!
+          </p>
+        </div>
+        <Toggler />
+      </div>
+    </main>
+  );
+};
+
+const Toggler = () => {
+  const [isChecked, setIsChecked] = React.useState(false);
+  return (
+    <div className="flex gap-4">
+      <div className="max-h-[481px] bg-primary rounded-lg w-full  flex flex-col">
+        <div className="flex p-4 ">
+          <label className="inline-flex items-center me-5 cursor-pointer">
+            <input
+              type="checkbox"
+              value=""
+              checked={isChecked}
+              onChange={() => setIsChecked((prev) => !prev)}
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-transparent border border-background rounded-full peer peer-focus:ring-4 peer-focus:ring-primaryDarker peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-background peer-checked:after:bg-primary after:border-primaryLighter after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-layout "></div>
+          </label>
+          <h2 className="uppercase text-background text-4xl">TRENUJĄCY</h2>
+        </div>
+        <div className="flex  justify-center items-end h-full">
+          <img className="h-[90%] " src="/assets/mission1.png" />
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-4">
+        <Step />
+        <Step />
+        <Step />
+      </div>
+    </div>
+  );
 };
 
 const Step = () => {
-	return (
-		<div className="bg-black rounded-lg p-8 flex flex-col gap-3 h-full">
-			<h5 className="font-bold text-xl">Krok 1</h5>
-			<p>Zaznacz jaki trening oferujesz oraz okreśł swoją dostępność!</p>
-		</div>
-	);
+  return (
+    <div className="bg-black rounded-lg p-8 flex flex-col gap-3 h-full">
+      <h5 className="font-bold text-xl">Krok 1</h5>
+      <p>Zaznacz jaki trening oferujesz oraz okreśł swoją dostępność!</p>
+    </div>
+  );
 };
+
+const STEPS = [
+  "Zaznacz jaki trening oferujesz oraz okreśł swoją dostępność!",
+  "Zaznacz jaki trening oferujesz oraz okreśł swoją dostępność!",
+  "Zaznacz jaki trening oferujesz oraz okreśł swoją dostępność!",
+];
